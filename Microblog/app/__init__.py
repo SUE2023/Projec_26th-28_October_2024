03712f3e24dde3a32195f1462034e264e33a2e88
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-10-26 12:12:28 +0000

    Config: added app.config.from_object(Config) method

--- Microblog/app/__init__.py
+++ Microblog/app/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/venv python3
 """ Make a package the directory"""
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
