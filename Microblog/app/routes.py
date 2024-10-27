1c979580fa698542100d19eeeaaa5f78ce2db467
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-10-27 07:50:51 +0000

    Added in routes.py: Login view function logic and Logout view function

--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -1,39 +1,31 @@
-#!/usr/bin/env python3
-""" Index Page Route"""
 
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm
+"""User Model """
+# ...
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user
 
-
-@app.route("/")
-@app.route("/index")
-def index():
-    user = {'username': 'Sophie'}
-    posts = [
-            {
-                'author': {'username':'Pauline'},
-                'body':'The African Bible'
-            },
-            {
-                'author': {'username': 'Vincentian'},
-                'body':'Christ is All'
-            },
-            {
-                'author': {'username': 'Vincentia'},
-                'body':'All Glory To Jesus'
-            }
-        ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
-
+# ...
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-        # return redirect('/index')
-        
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
---
+++ Microblog/app/routes1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Index Page Route"""
+
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {'username': 'Sophie'}
+    posts = [
+            {
+                'author': {'username':'Pauline'},
+                'body':'The African Bible'
+            },
+            {
+                'author': {'username': 'Vincentian'},
+                'body':'Christ is All'
+            },
+            {
+                'author': {'username': 'Vincentia'},
+                'body':'All Glory To Jesus'
+            }
+        ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+        # return redirect('/index')
+        
+    return render_template('login.html', title='Sign In', form=form)
