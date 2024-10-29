b1ae538db5ee594e0ad1f2e782288be8cbb398ae
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-10-29 04:44:45 +0000

    Logging to a file:added RotatingFileHandler

--- Microblog/app/__init__.py
+++ Microblog/app/__init__.py
@@ -7,6 +7,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
@@ -17,6 +19,7 @@ login = LoginManager(app)
 login = LoginManager(app)
 login.login_view = 'login'
 if not app.debug:
+    """Loging to mail"""
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -31,6 +34,19 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    """Login to file"""
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 
 from app import routes, models, errors
