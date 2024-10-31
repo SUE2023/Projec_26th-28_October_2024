04af2b4f09af67dd8aaaa76cc96afbfee6f00285
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-10-31 05:45:36 +0000

    Reset password request view function.

--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -12,6 +12,8 @@ from urllib.parse import urlsplit
 from app.forms import RegistrationForm
 from datetime import datetime, timezone
 from app.forms import EditProfileForm
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 
 # ...
@@ -96,3 +98,18 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.email == form.email.data))
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
